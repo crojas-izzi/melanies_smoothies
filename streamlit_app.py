f16d4fb8e0abea5397dd233e035f28f2aa7089e6
crojas-izzi <p-crojasd@izzi.mx>
2025-07-17 17:17:58 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,5 +51,5 @@ if ingredients_list:
 # Nueva sección para mostrar información nutricional desde SmoothieFroot
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.jonson())
 
